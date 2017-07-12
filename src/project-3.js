12aeafca378d4ce82a8258c37e482ef00556955d
mongeproductions <mongeproductions@gmail.com>
2017-07-11 22:12:18 -0600

    Submitting work done on projects 2 and 3

--- src/project-3.js
+++ src/project-3.js
@@ -5,44 +5,68 @@ const makeCat = (name, age) => {
   // add an age property to the object with the value set to the age argument
   // add a method called meow that returns the string 'Meow!'
   // return the object
+const newobject={
+  name:name,
+  age:age
+};
+newobject.meow=()=>'Meow!';
+return newobject
 };
 
 const addProperty = (object, property) => {
   // add the property to the object with a value of null
   // return the object
   // note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+object[property]=null;
+return object
 };
 
 const invokeMethod = (object, method) => {
   // method is a string that contains the name of a method on the object
   // invoke this method
   // nothing needs to be returned
+  object[method]();
 };
 
 const multiplyMysteryNumberByFive = (mysteryNumberObject) => {
   // mysteryNumberObject has a property called mysteryNumber
   // multiply the mysteryNumber property by 5 and return the product
+return mysteryNumberObject.mysteryNumber*5;
 };
 
 const deleteProperty = (object, property) => {
   // remove the property from the object
   // return the object
+  delete object[property];
+  return object;
 };
 
 const newUser = (name, email, password) => {
   // create a new object with properties matching the arguments passed in.
   // return the new object
+  const newobject={
+    name:name,
+    email:email,
+    password:password
+  };
+  return newobject;
 };
 
 const hasEmail = (user) => {
   // return true if the user has a value for the property 'email'
   // otherwise return false
+
 };
 
 const hasProperty = (object, property) => {
   // return true if the object has the value of the property argument
   // property is a string
   // otherwise return false
+  if (object.hasOwnProperty(property)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const verifyPassword = (user, password) => {
